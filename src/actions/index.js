90a6ed310e2b5f66c540a0d3f2058b495da6ad33
Trevor Malloy <trevoramalloy@gmail.com>
2017-12-21 11:49:53 -0800

    Set up route refresh on submit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=TMREDUXBLOGAPI';
 
+// Action to GET posts
 export function fetchPosts() {
 const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+// Action to POST posts
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
