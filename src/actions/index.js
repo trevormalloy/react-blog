3d633b41a91bd41858eab5928c1090c41fc61553
Trevor Malloy <trevoramalloy@gmail.com>
2017-12-21 23:56:58 -0800

    Action creators, reducers, post show, and of course how to delete a post.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=TMREDUXBLOGAPI';
@@ -26,3 +28,23 @@ export function createPost(values, callback) {
     payload: request
   };
 }
+
+// Get a specific post by the id
+ export function fetchPost(id) {
+   const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+   return {
+     type: FETCH_POST,
+     payload: request
+   };
+ }
+
+ export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+      .then(() => callback());
+
+    return {
+      type: DELETE_POST,
+      payload: id
+    }
+ }
